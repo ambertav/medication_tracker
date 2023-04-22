50c1b983b227214c92b79b74732601c429eacf73
Amber Taveras <taveras.amber@gmail.com>
2023-04-22 11:53:17 -0400

    implements user signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 
 def home (request) :
     return HttpResponse('Hello world')
+
+def signup (request) :
+    # handling POST request
+    if request.method == 'POST' :
+        form = UserCreationForm(request.POST)
+        if form.is_valid() :
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        
+    # handling GET request
+    form = UserCreationForm()
+    return render(request, 'registration/signup.html', {
+        'form': form
+    })
