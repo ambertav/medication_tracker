bd44b85c6e7d5dd3e967d7dbc54b3b4888b16736
Amber Taveras <taveras.amber@gmail.com>
2023-04-26 23:25:18 -0400

    adds delete dose and inactivate/activate medications

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,11 +9,16 @@ urlpatterns = [
     path('patients/<int:pk>/update/', views.PatientUpdate.as_view(), name='patient_update'),
     path('patients/<int:pk>/delete/', views.PatientDelete.as_view(), name='patient_delete'),
 
-    path('medications/<int:medication_id>/dose/create/', views.dose_create, name='dose_create'),
     path('medications/<int:medication_id>/', views.medication_detail, name='medication_detail'),
     path('medications/create/', views.medication_create, name='medication_create'),
     path('medications/<int:pk>/update/', views.MedicationUpdate.as_view(), name='medication_update'),
     path('medications/<int:pk>/delete/', views.MedicationDelete.as_view(), name='medication_delete'),
+
+    path('medications/<int:medication_id>/inactivate/', views.medication_inactivate, name='medication_inactivate'),
+    path('medications/<int:medication_id>/activate/', views.medication_activate, name='medication_activate'),
+
+    path('medications/<int:medication_id>/dose/create/', views.dose_create, name='dose_create'),
+    path('medications/<int:medication_id>/dose/<int:dose_id>/delete/', views.dose_delete, name='dose_delete'),
 
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Patient, Medication, Dose
 from .forms import MedForm, DoseInlineFormset
 
+from datetime import date
+
 # Create your views here.
 
 # general views
@@ -27,7 +29,7 @@ def signup (request) :
     # handling GET request
     form = UserCreationForm()
     return render(request, 'registration/signup.html', {
-        'form': form
+        'form': form,
     })
 
 
@@ -36,7 +38,7 @@ def signup (request) :
 def patient_list (request) :
     patients = Patient.objects.filter(user=request.user).order_by('name')
     return render(request, 'patients/patient_list.html', {
-        'patients': patients
+        'patients': patients,
     })
 
 @login_required
@@ -44,6 +46,7 @@ def patient_detail (request, patient_id) :
     patient = Patient.objects.filter(user=request.user).get(id=patient_id)
     return render(request, 'patients/patient_detail.html', {
         'patient': patient,
+        'date': date,
     })
 
 class PatientCreate (LoginRequiredMixin, CreateView) :
@@ -71,14 +74,14 @@ class PatientDelete (LoginRequiredMixin, DeleteView) :
     
 
 # medication views
-
 @login_required
 def medication_detail (request, medication_id) :
     medication = Medication.objects.filter(user=request.user).get(id=medication_id)
     dose_form = DoseInlineFormset()
     return render(request, 'medications/medication_detail.html', {
         'medication': medication,
-        'dose_form': dose_form
+        'dose_form': dose_form,
+        'date': date,
     })
 
 @login_required
@@ -88,11 +91,18 @@ def medication_create (request) :
         dose_form = DoseInlineFormset(request.POST)
         med_form = MedForm(request.POST)
         if med_form.is_valid() and dose_form.is_valid () :
+            # if med_form.instance.day_supply == None :
+            #     times_per_day = 24 / dose_form.time_interval
+            #     amount_per_day = dose_form.amount * times_per_day
+            #     day_supply = med_form.instance.quantity / amount_per_day
+            #     med_form.instance.day_supply = day_supply
             med_form.instance.user = request.user
             instance = med_form.save()
             dose_form.instance = instance
             dose_form.save()
-            return redirect('patient_list')
+            return redirect('patient_detail', {
+                'patient_id': instance.patient.id
+                })
 
     # handling GET request
     med_form = MedForm()
@@ -103,17 +113,23 @@ def medication_create (request) :
     })
 
 @login_required
-def dose_create (request, medication_id) :
-    dose_form = DoseInlineFormset(request.POST)
+def medication_inactivate (request, medication_id) :
+    medication = Medication.objects.get(id=medication_id)
+    medication.is_active = False
+    medication.inactive_date = date.today().isoformat()
+    medication.save()
+    return redirect('patient_detail', patient_id=medication.patient.id)
 
-    if dose_form.is_valid () :
-        dose_form.instance = Medication.objects.get(id=medication_id)
-        dose_form.save()
-
-    return redirect('medication_detail', medication_id=medication_id)
+@login_required
+def medication_activate (request, medication_id) :
+    medication = Medication.objects.get(id=medication_id)
+    medication.is_active = True
+    medication.inactive_date = None
+    medication.save()
+    return redirect('patient_detail', patient_id=medication.patient.id)
 
 class MedicationUpdate (LoginRequiredMixin, UpdateView) :
-    fields = ('name', 'day_supply')
+    fields = ('name', 'quantity', 'unit', 'day_supply', 'start_date', 'refills', 'patient')
     template_name = 'medications/medication_form.html'
 
     def get_queryset (self) :
@@ -128,3 +144,29 @@ class MedicationDelete (LoginRequiredMixin, DeleteView) :
     def get_success_url (self) :
         patient = self.object.patient 
         return reverse_lazy('patient_detail', kwargs={'patient_id':patient.id})
+    
+
+# dose views
+@login_required
+def dose_create (request, medication_id) :
+    dose_form = DoseInlineFormset(request.POST)
+
+    if dose_form.is_valid () :
+        dose_form.instance = Medication.objects.get(id=medication_id)
+        dose_form.save()
+
+    return redirect('medication_detail', medication_id=medication_id)
+
+@login_required
+def dose_delete (request, medication_id, dose_id) :
+    dose = Dose.objects.get(id=dose_id)
+    medication = Medication.objects.get(id=medication_id)
+    
+    if request.method == 'POST' :
+        dose.delete()
+        return redirect('medication_detail', medication_id=medication.id)
+    
+    return render(request, 'doses/dose_confirm_delete.html', {
+        'dose': dose,
+        'medication': medication
+    })
